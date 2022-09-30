8d18bf2429efb6eaf0acae18509b029c81fe1502
MennaKB <110547207+MennaKB@users.noreply.github.com>
2022-09-30 15:06:52 +0200

    Add files via upload

---
+++ Ecommerce(modified)V1.0.1/pdp.js
@@ -0,0 +1,36 @@
+const imgs = document.querySelectorAll('.img-select a');
+const imgBtns = [...imgs];
+let imgId = 1;
+
+imgBtns.forEach((imgItem) => {
+    imgItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        imgId = imgItem.dataset.id;
+        slideImage();
+    });
+});
+
+function slideImage(){
+    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+
+    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+}
+
+window.addEventListener('resize', slideImage);
+
+function addToCartClicked(event) {
+    if (sessionStorage.getItem('status') != null){
+
+    <p>Please follow the link<a href="login.html">to login first</a>.</p>
+
+    }
+    else {
+    let button = event.target;
+    let shopItem = button.parentElement.parentElement;
+    let title = shopItem.getElementsByClassName('shop-item-title')[0].innerText;
+    let price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
+    let imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src;
+    addItemToCart(title, price, imageSrc);
+    updateCartTotal();
+    }
+}
